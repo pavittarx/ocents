b6028e3983c0de3696581924d9eb756222d43c61
pavittarx <pavittarx@users.noreply.github.com>
2020-05-28 17:36:01 +0530

    unwanted imports removed

--- scalars.js
+++ scalars.js
@@ -1,7 +1,4 @@
 // For graphql-codegen
 let {typeDefs} = require("graphql-scalars");
-const { buildSchema } = require('graphql');
-
 typeDefs = typeDefs.reduce((a, c) => a+c+' ', '');
-
 module.exports = typeDefs;
