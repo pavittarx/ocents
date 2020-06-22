cbe2da1503083fbcfc7c2b626ac08685f4782b49
pavittarx <pavittarx@users.noreply.github.com>
2020-06-22 09:25:34 +0530

    server deployment fix

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,8 +3,8 @@ import styles from "@/styles/pages/index.module.scss";
 
 function Index(){
   return <> 
-  <h1> This is the main page. </h1>
-  <Link href="/about"><a> About Page</a></Link>
+  <h1> Ocents </h1>
+  <p> Coming Soon... </p> 
   </>;
 }
 
