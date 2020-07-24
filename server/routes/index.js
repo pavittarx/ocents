c96246af8bfb08ee8c8012bfec1a0aeb460f88cf
Prabhat Raushan <prabhatraush@gmail.com>
2020-07-24 22:29:23 +0530

    user login added

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const {Users} = require("./../models/index");
 
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
 
 router.post("/signup", async (req, res) => {
 
-  let usersParams = req.body;
+  let userParams = req.body;
 
-  if(!usersParams.email || !usersParams.password)
+  if(!userParams.email || !userParams.password)
   {
     res
       .status(401)
@@ -17,7 +17,7 @@ router.post("/signup", async (req, res) => {
       return;
   }
 
-  let existUser = await Users.findOne({email: usersParams.email});
+  let existUser = await Users.findOne({email: userParams.email});
   if(existUser)
   {
     res
@@ -27,9 +27,9 @@ router.post("/signup", async (req, res) => {
     return;
   }
 
-  let hashPwd = await bcrypt.hash(usersParams.password, 12);
-  usersParams.password = hashPwd;
-  let user = new Users(usersParams);
+  let hashPwd = await bcrypt.hash(userParams.password, 12);
+  userParams.password = hashPwd;
+  let user = new Users(userParams);
 
   user = await user.save();
 
@@ -41,7 +41,38 @@ router.post("/signup", async (req, res) => {
        name: user.name
     });
   }
-  
+});
+
+
+router.post("/login", async(req, res)=>{
+  let userParams = req.body;
+
+  if(!userParams.email || !userParams.password)
+  {
+    res.status(401).send("Please provide both email and password.");
+    return;
+  }
+
+  let user = await Users.findOne({email:userParams.email})
+  if(!user)
+  {
+    res.status(401).send("The given user does not exist.").end();
+    return;
+  }
+
+  // checking the credentials for user
+  if( !(await bcrypt.compare(userParams.password, user.password)))
+  {
+    res.status(401).send("Incorrect credentials!").end();
+    return;
+  }
+
+  let token = await jwt.sign(
+    {id: user._id, email: user.email, name: user.name},
+    user.password
+  );
+
+  res.cookie("token", token).send("Login Success");
 });
 
 module.exports = router;
